ba87cd4b139c70562f07165d288d89cdaa036031
tomasel <elia.tomasi@pm.me>
2022-02-11 17:37:37 +0100

    robe sono state fatte, nulla di troppo utile

--- games/e_2048.c
+++ games/e_2048.c
@@ -1,40 +1,59 @@
 #include "libraries/init.h"
 #include "assets/e_2048.h"
-
-// colors
-#define E_BACK 0xBBADA0
-#define E_4    0x4495D4
-#define E_8    0x2F6895
-#define E_16   0xF5BD70
-#define E_32   0xF2A032
-#define E_64   0xCD8829
-#define E_128  0xE37051
-#define E_256  0xDE5833
-#define E_512  0xBD4A2B
-#define E_1024 0x5454DA
-#define E_2048 0xDA54D8
+#include <string.h>
 
 // table
-#define E_Tbl_origin 32
-#define E_Tbl_end    SCREEN_MAX_X-1
-#define E_Tbl_hgt    96
-#define E_Tbl_row    E_Tbl_hgt/4
-Graphics_Rectangle table = {E_Tbl_origin,E_Tbl_origin,E_Tbl_end,E_Tbl_end};
+#define E_screenCln 24
+#define E_screenRow 24
+#define E_screenOff 32
+#define E_screenEnd SCREEN_MAX_X
+Graphics_Rectangle e_table = {E_screenOff,E_screenOff,E_screenEnd,E_screenEnd};
+int arr_table[4][4] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};    // contiene i valori che il giocatore può vedere
+
+// colors
+uint32_t colors[] = {E_2,
+                     E_4,
+                     E_8,
+                     E_16,
+                     E_32,
+                     E_64,
+                     E_128,
+                     E_256,
+                     E_512,
+                     E_1024,
+                     E_2048,
+                     ClrBlack};
+
+char* e_numbers_str[] = {"2","4","8","16","32","64","128","256","512","1024","2048","4096","8192","16K","32K","64K","128K"};
 
 // functions
 void e_cls (void);
 void e_drawLogo (void);
 void e_drawTable (void);
 void e_drawScore (void);
+void e_random2 (void);
+void e_endgame (void);
+
+// flags
+short e_occu = 0;     // currently occupied square
 
 // main
 void e_2048 () {
-    Graphics_setBackgroundColor(&ctx,E_BACK);
+    srand(millis());
     e_cls();
     e_drawLogo();
     e_drawTable();
     while (1) {
+        // genera nuovo 2
+        e_random2();
+        // aspettare input
 
+        // aggiornare stato
+        if (e_occu == 16) {
+            e_endgame();
+        } else {
+            // ...
+        }
     }
 }
 
@@ -48,11 +67,42 @@ void e_drawLogo(void) {
 }
 
 void e_drawTable(void) {
-    Graphics_drawRectangle(&ctx,&table);
+    Graphics_drawRectangle(&ctx,&e_table);
+    /*
     Graphics_drawLineH(&ctx,E_Tbl_origin,E_Tbl_end,E_Tbl_end - 3 * E_Tbl_row);
     Graphics_drawLineH(&ctx,E_Tbl_origin,E_Tbl_end,E_Tbl_end - 2 * E_Tbl_row);
     Graphics_drawLineH(&ctx,E_Tbl_origin,E_Tbl_end,E_Tbl_end - E_Tbl_row);
     Graphics_drawLineV(&ctx,E_Tbl_end - E_Tbl_row,E_Tbl_origin,E_Tbl_end);
     Graphics_drawLineV(&ctx,E_Tbl_end - 2 * E_Tbl_row,E_Tbl_origin,E_Tbl_end);
     Graphics_drawLineV(&ctx,E_Tbl_end - 3 * E_Tbl_row,E_Tbl_origin,E_Tbl_end);
+    */
+    Graphics_drawLineH(&ctx,E_screenOff,E_screenEnd,E_screenOff + E_screenRow);
+    Graphics_drawLineH(&ctx,E_screenOff,E_screenEnd,E_screenOff + 2 * E_screenRow);
+    Graphics_drawLineH(&ctx,E_screenOff,E_screenEnd,E_screenOff + 3 * E_screenRow);
+    Graphics_drawLineV(&ctx,E_screenOff + E_screenCln,E_screenOff,E_screenEnd);
+    Graphics_drawLineV(&ctx,E_screenOff + 2 * E_screenCln,E_screenOff,E_screenEnd);
+    Graphics_drawLineV(&ctx,E_screenOff + 3 * E_screenCln,E_screenOff,E_screenEnd);
+}
+
+void e_random2 (void) {
+    int x, y;
+    if (e_occu < 16) {
+        while (1) {
+            x = rand() % 4;
+            y = rand() % 4;
+            if (arr_table[x][y] == 0) {
+                Graphics_drawImage(&ctx,
+                                   &e_2_img,
+                                   x * E_screenCln + E_screenOff + 1,
+                                   y * E_screenRow + E_screenOff + 1);
+                arr_table[x][y] = 2;
+                ++e_occu;
+                return;
+            }
+        }
+    }
+}
+
+void e_endgame (void) {
+    // screen of death
 }
